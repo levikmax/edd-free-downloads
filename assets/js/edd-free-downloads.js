8403d5d3a4a80fe6a7e9659d8df7fc49ca015cf5
John Parris <public@johnparris.com>
2015-10-09 12:27:14 -0400

    Close modal after 2 secs, which helps when auto-download is enabled.

--- assets/js/edd-free-downloads.js
+++ assets/js/edd-free-downloads.js
@@ -21,7 +21,8 @@ jQuery(document).ready(function ($) {
             attach: $('.edd-free-download'),
             content: $('#edd-free-downloads-modal'),
             width: 350,
-            closeButton: edd_free_downloads_vars.close_button
+            closeButton: edd_free_downloads_vars.close_button,
+            delayClose: 2000
         });
 
         $('.edd-free-download').click(function (e) {
@@ -138,6 +139,7 @@ jQuery(document).ready(function ($) {
             $('.edd-free-download-submit span').html(edd_free_downloads_vars.download_loading);
             $('.edd-free-download-submit span').append('<i class="edd-icon-spinner edd-icon-spin"></i>');
             $('.edd-free-download-submit').attr('disabled', 'disabled');
+            newModal.close();
         } else {
             $('.edd-free-download-errors').css('display', 'block');
         }
